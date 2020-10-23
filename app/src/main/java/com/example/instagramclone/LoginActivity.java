c282197fbdf5a5b47f57fb11f4606999e0f86877
Heremy <espinosaJ99@gmail.com>
2020-10-23 15:13:34 -0700

    When user logs in they are taken to MainActivity

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -2,11 +2,16 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +25,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if(ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
@@ -30,8 +39,30 @@ public class LoginActivity extends AppCompatActivity {
                 Log.i(TAG,"onClick for login button");
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
+                loginUser(username,password);
             }
         });
 
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login in user " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Issue with login",e);
+                    return;
+                }
+                // Navigate to main acitivity if user signed in properly
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
