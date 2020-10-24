bf782a265b8f1c87eaed7c1e4a103255c91877fe
Heremy <espinosaJ99@gmail.com>
2020-10-23 20:34:30 -0700

    User can now create an account from LoginActivity

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -8,10 +8,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +47,42 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG,"onClick for signup");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signupUser(username,password);
+            }
+        });
+
+    }
+
+    private void signupUser(String username, String password) {
+        if(username.isEmpty()) {
+            Toast.makeText(LoginActivity.this,"Need Username to signup.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(password.isEmpty()) {
+            Toast.makeText(LoginActivity.this,"Account must have password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null) {
+                    Log.e(TAG,"Issue creating an account",e);
+                    return;
+                }
+                Log.i(TAG,"Account created succesfully");
+                Toast.makeText(LoginActivity.this,"Account created!", Toast.LENGTH_SHORT).show();
+                goMainActivity();
+            }
+        });
     }
 
     private void loginUser(String username, String password) {
@@ -54,6 +94,7 @@ public class LoginActivity extends AppCompatActivity {
                     Log.e(TAG, "Issue with login",e);
                     return;
                 }
+                Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
                 // Navigate to main acitivity if user signed in properly
                 goMainActivity();
             }
