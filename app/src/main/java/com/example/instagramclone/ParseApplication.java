76d3c38d81782ed1441f0ed97dd373b7ecf937f0
Heremy <espinosaJ99@gmail.com>
2020-10-23 17:06:32 -0700

    User can now take a picture and upload post to Back4app

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,12 +3,15 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("LSCvzvRcmP9m1cvEstXYTgtwJ4dofjlo36LF7OKs")
